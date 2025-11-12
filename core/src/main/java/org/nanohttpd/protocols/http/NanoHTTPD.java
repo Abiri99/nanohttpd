467b05ee906714b3d80b49989ce3632c82dd98c6
Alireza Abiri <abiriali78@gmail.com>
2025-11-12 16:10:06 +0100

    Fix typo in NanoHTTPD.java
    Replaces 'socet' with 'socket' on line 415.

--- core/src/main/java/org/nanohttpd/protocols/http/NanoHTTPD.java
+++ core/src/main/java/org/nanohttpd/protocols/http/NanoHTTPD.java
@@ -412,7 +412,7 @@ public abstract class NanoHTTPD {
      * provide a subclass of the ServerRunnable.
      * 
      * @param timeout
-     *            the socet timeout to use.
+     *            the socket timeout to use.
      * @return the server runnable.
      */
     protected ServerRunnable createServerRunnable(final int timeout) {
